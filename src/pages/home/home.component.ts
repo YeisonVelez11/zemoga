256efd675c1f740ef74073e75ba7860c32e6bfc9
yeison.velez@cargogistix.com <yeison.velez@cargogistix.com>
2021-07-21 01:07:25 -0500

    Responsive dropdown filter

--- src/pages/home/home.component.ts
+++ src/pages/home/home.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { WEBSERVICE, URL } from '../../config/webservices';
 import { ServicesProvider } from '../../providers/services';
+import Swiper from 'swiper';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit {
+  swiper: any; //is necesary to use the destroy function
   dropDownGrid: boolean = true; //controls if dropown should be open/closed
-  valueDropdownSelected: string = 'List';
-
+  valueDropdownSelected: string = 'Grid';
+  deviceScreenPhone: boolean; //check if resolution is phone
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.getDeviceScreen();
+  }
   constructor(private ServicesProvider: ServicesProvider) {}
   ngOnInit() {
     // this.ServicesProvider.preloaderOn();
+    console.log(window.innerWidth);
+    this.getDeviceScreen();
   }
   actionDropdown() {
     this.dropDownGrid = !this.dropDownGrid;
@@ -19,5 +28,32 @@ export class HomeComponent implements OnInit {
   setOptionDropdownGrid(type) {
     this.valueDropdownSelected = type;
     this.dropDownGrid = true;
+  }
+
+  getDeviceScreen() {
+    if (window.innerWidth < 499) {
+      if (!this.deviceScreenPhone) {
+        //prevent multiple calls to start swipper
+        this.startSwipper();
+        this.deviceScreenPhone = true;
+      }
+    } else {
+      if (this.deviceScreenPhone) {
+        //prevent multiple calls to destroy swipper
+        this.deviceScreenPhone = false;
+        if (this.swiper) {
+          this.swiper.destroy();
+        }
+      }
+    }
+  }
+
+  startSwipper() {
+    this.swiper = new Swiper('.swiper-container', {
+      freeMode: false,
+      initialSlide: 0,
+      centerInsufficientSlides: true,
+      loop: false,
+    });
   }
 }
